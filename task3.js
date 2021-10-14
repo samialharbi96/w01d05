8b837547973682689bd83adb62cfd0edb28bd621
samialharbi96 <sami.sagier.js@tuwaiq.edu.sa>
2021-10-14 20:59:15 +0300

    task4

--- task3.js
+++ task3.js
@@ -1,5 +1,34 @@
-let num1 = 4
-let num2 = 2
-let num3 = 6
+let num1=5;
+let num2=11;
+let num3=7;
+
+if (num1 > num2) { console.log( "The first number is big");}
+else if(num1 < num2){
+    console.log("The second number is greater than the first");}
+else{console.log("The first and second numbers have the same value"); }
+
+if(num1 % 2 == 0){
+    console.log("The first number is an even number");}
+else{
+    console.log("The first number is odd");}
+
+if (num1 > num2 && num1 > num3) {if (num2 > num3) 
+    {console.log(`${num1} > ${num2} > ${num3}`);}
+     else {console.log(`${num1} > ${num3} > ${num2}`);}
+} 
+else if (num2 > num1 && num2 > num3) {if (num1 > num3) {
+    console.log(`${num2} > ${num1} > ${num3}`); } 
+    else {
+    console.log(`${num2} > ${num3} > ${num1}`);}} else {
+  if (num1 > num2) {console.log(`${num3} > ${num1} > ${num2}`);}
+
+else {
+    console.log(`${num3} > ${num2} > ${num}`);}}
+
+
+for (let i = 0; i < 21; i++) {
+    console.log(i);}
+  
+
 
 
