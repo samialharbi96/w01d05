96de2271db9966fea21319ffa8f195cb7789ecef
samialharbi96 <sami.sagier.js@tuwaiq.edu.sa>
2021-10-14 18:54:12 +0300

    mytask2

---
+++ task2.js
@@ -0,0 +1,21 @@
+let integer = 10 
+let string = " this String "
+
+
+let rray = ["first" ,8 ,11]
+
+
+const myName ={
+    Name: "sami",
+    Favoritecolor:"red",
+  Weight: 61,
+  height: 167
+}
+
+console.log(typeof integer)
+
+console.log(typeof string)
+
+console.log(typeof rray)
+
+console.log(typeof myName)
