f955d5dae31974e889c7b5286a50ddd7318ecf2b
samialharbi96 <sami.sagier.js@tuwaiq.edu.sa>
2021-10-14 12:22:08 +0300

    task1

---
+++ task1.js
@@ -0,0 +1,13 @@
+let language ="JavaScript";
+let score =10
+console.log(language);
+console.log(score);
+let widh = "30"
+let length =" 20"
+let area = widh * length
+console.log(area);
+let num1 = 3 ;
+let  num2 =2 ;
+let x = num1 % num2 ;
+console.log(x);
+let y = num1 / num2 ;
